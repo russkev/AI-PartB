638c85a9c52e1b0913db744ee46e42449fe6d4ab
russkev <russkev@gmail.com>
2021-04-27 22:10:37 +1000

    Cleaned up mcts_regret, removed commented code

--- src/state/node_mcts_regret.py
+++ src/state/node_mcts_regret.py
@@ -6,7 +6,6 @@ David Peel 964682
 Kevin Russell 1084088
 """
 
-from state.location import distance
 from state.game_state_fast import GameState
 
 
@@ -79,9 +78,6 @@ class Node(GameState):
         unvisited = []
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[0])):
-                # child: Node = self.matrix[i][j]
-                # if self.matrix[i][j] is None:
-                #     self.matrix[i][j] = self.make_updated_node(self.friend_transitions, self.enemy_transitions, self)
                 if self.matrix[i][j].num_visits == 0:
                     unvisited.append(self.matrix[i][j])
         return unvisited
@@ -120,160 +116,8 @@ class Node(GameState):
                 updated_node.parent = parent
             return updated_node
 
-    # def __init_arrays(self):
-    #     """
-    #     Initialize matrix and regret array with correct sizes
-    #     """
-    #     self.friend_transitions = self.next_transitions_for_side(is_friend=True)
-    #     self.enemy_transitions = self.next_transitions_for_side(is_friend=False)
-    #     self.matrix = [
-    #         [None for _ in range(len(self.enemy_transitions))]
-    #         for _ in range(len(self.friend_transitions))]
-
-    #     self.regret = [0 for _ in range(len(self.friend_transitions))]
-
-
     def __repr__(self):
         """
         Display the value / num_visits. This is nice and small so fits into a matrix.
         """
         return f"{self.q_value}/{self.num_visits}"
-
-
-# class Node(GameState):
-#     MAX_DISTANCE = 2
-
-#     def __init__(self, other: GameState):
-#         """
-#         Initialise with a GameState.
-
-#         Set is_friend parameter to false since our starting position (root) will always be the case 
-#         where enemy has just moved and we're choosing where to move. This is making the (incorrect)
-#         assumption that moves are sequential instead of simultaneous.
-#         """
-#         super().__init__()
-#         self.is_upper = other.is_upper
-#         self.friends = other.friends
-#         self.enemies = other.enemies
-#         self.turn = other.turn
-#         self.friend_throws = other.friend_throws
-#         self.enemy_throws = other.enemy_throws
-
-#         self.action = None  # Action made to make this move
-#         self.parent = None
-#         # self.is_friend = False
-#         self.is_visited = False
-#         self.is_fully_expanded = False
-
-#         self.q_value = 0
-#         self.num_visits = 0
-
-#         self.friend_transitions = []
-#         self.enemy_transitions = []
-#         self.regret = []
-#         self.matrix = [[]]
-#         self.i = 0
-#         self.j = 0
-
-#     def init_transitions(self):
-#         self.friend_transitions = self.next_friend_moves()
-#         self.friend_transitions += self.__prune_throws(
-#             self.next_friend_throws(), True)
-
-#         self.enemy_transitions = self.next_enemy_moves()
-#         self.enemy_transitions += self.__prune_throws(
-#             self.next_enemy_throws(), False)
-
-#         self.init_matrix()
-
-#     # def __prune_throws(self, throws, is_friend):
-#     #     throw_enemies = self.enemies if is_friend else self.friends
-#     #     pruned_throws = []
-
-#     #     if is_friend:
-#     #         tokens_remaining = self.friend_throws
-#     #     else:
-#     #         tokens_remaining = self.enemy_throws
-#     #     if (is_friend and self.is_upper) or (not is_friend and not self.is_upper):
-#     #         pruned_throws += Node.__append_throws_distant(
-#     #             throws, tokens_remaining, throw_enemies, True)
-#     #     else:
-#     #         pruned_throws += Node.__append_throws_distant(
-#     #             throws, tokens_remaining, throw_enemies, False)
-
-#     #     if len(pruned_throws) == 0:
-#     #         # There are opposing tokens less than MAX_DISTANCE away
-#     #         for throw in throws:
-#     #             (_, _, throw_loc) = throw
-#     #             for (_, enemy_loc) in throw_enemies:
-#     #                 if distance(throw_loc, enemy_loc) <= Node.MAX_DISTANCE:
-#     #                     pruned_throws.append(throw)
-#     #                     break
-
-#     #     return pruned_throws
-
-#     # @staticmethod
-#     # def __append_throws_distant(throws, tokens_remaining, throw_enemies, is_upper):
-#     #     # farthest_r = -4 if is_upper else 4
-#     #     farthest_r = GameState.farthest_r(tokens_remaining, is_upper)
-#     #     nearest_throw_enemy_r = -4 if is_upper else 4
-#     #     pruned_throws = []
-#     #     for (_, (r, _)) in throw_enemies:
-#     #         if r > nearest_throw_enemy_r:
-#     #             nearest_throw_enemy_r = r
-#     #     if abs(farthest_r - nearest_throw_enemy_r) > Node.MAX_DISTANCE:
-#     #         for throw in throws:
-#     #             (_, _, (throw_r, _)) = throw
-#     #             if throw_r == farthest_r:
-#     #                 pruned_throws.append(throw)
-#     #     return pruned_throws
-
-
-
-#     def has_unvisited_children(self):
-#         if self.is_fully_expanded:
-#             return False
-#         else:
-#             for friend_index in range(len(self.friend_transitions)):
-#                 for enemy_index in range(len(self.enemy_transitions)):
-#                     if self.matrix[friend_index][enemy_index] is None:
-#                         return True
-#             self.is_fully_expanded = True
-#             return False
-
-#     def update_node(self, friend_transition, enemy_transition, parent=None):
-#         """
-#         If the new state already exists in the matrix, return that, otherwise return a fresh node
-
-#         the parent argument is for updating a simulation inside MCTS. If this is used as the actual
-#         update method, parent should stay default None
-#         """
-#         friend_index = enemy_index = -1
-#         if parent is None:
-#             # Find new root, parent will be None in this case
-#             for i, curr_friend_transition in enumerate(self.friend_transitions):
-#                 if curr_friend_transition == friend_transition:
-#                     friend_index = i
-
-#             for j, curr_enemy_transition in enumerate(self.enemy_transitions):
-#                 if curr_enemy_transition == enemy_transition:
-#                     enemy_index = j
-
-#         if friend_index > -1 and enemy_index > -1:
-#             # Correct child node found
-#             updated_node: Node = self.matrix[friend_index][enemy_index]
-#             updated_node.parent = None
-#             return updated_node
-#         else:
-#             # New node needs to be created
-#             updated_node = self.copy_node_state()
-#             updated_node.update(friend_transition, enemy_transition)
-#             if parent is not None:
-#                 updated_node.parent = parent
-#             return updated_node
-
-#     def __repr__(self):
-#         """
-#         Display the value / num_visits. This is nice and small so fits into a matrix.
-#         """
-#         return f"{self.q_value}/{self.num_visits}"
--- src/strategy/mcts_regret.py
+++ src/strategy/mcts_regret.py
@@ -36,12 +36,8 @@ def monte_carlo_tree_search(root: Node, num_iterations) -> Node:
     The following pseudo code was used, with modifications for simultaneous move play.
     https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
     """
-
-    # if len(root.friend_transitions) == 0 and len(root.enemy_transitions) == 0:
-    #     root.init_transitions()
     root.parent = None
 
-    # for _ in range(num_iterations):
     while (root.num_visits < num_iterations):
         # A leaf node of the current frontier, does not include nodes visited in the rollout stage
         leaf = traverse(root)
@@ -63,9 +59,6 @@ def traverse(node: Node):
 
     Pick a chiled from which to apply a rollout from the unvisited children.
     """
-
-    # while not node.has_unvisited_children():
-    #     node = select(node)
     while node.is_fully_expanded:
         node = get_best_child(node)
 
@@ -82,16 +75,11 @@ def back_propagate(node: Node, reward):
     For all nodes that formed part of the branch which just had a rollout, 
     update the statistics for those nodes
     """
-    # if node.parent is None:
-    #     return
-    # node.num_visits += 1
-    # node.q_value += reward
-    # update_regret(node, reward)
-    # back_propagate(node.parent, reward)
     update_stats(node, reward)
     if node.parent is not None:
         update_regret(node, reward)
         back_propagate(node.parent, reward)
+
 
 def update_stats(node: Node, result):
     """
@@ -99,54 +87,6 @@ def update_stats(node: Node, result):
     """
     node.num_visits += 1
     node.q_value += result
-
-
-####################################################################################################
-#####-------------------------- END TRADITIONAL MCTS IMPLEMENTATION ---------------------------#####
-####################################################################################################
-
-
-# def sm_mcts(root: Node, num_iterations) -> Node:
-#     """
-#     Entry point for Monte Carlo Tree Search algorithm as implemented in the following paper:
-#     https://papers.nips.cc/paper/2013/file/1579779b98ce9edb98dd85606f2c119d-Paper.pdf
-
-#     It functions the same way but is ordered a bit of a different way than the "traditional"
-#     approach
-#     """
-#     if len(root.friend_transitions) == 0 and len(root.enemy_transitions) == 0:
-#         root.init_transitions()
-
-#     for _ in range(num_iterations):
-#         iterate(root)
-
-#     child = select(root)
-#     return child
-
-
-# def iterate(root: Node):
-#     """
-#     Run a single iteration of the algorithm and update the root and any children of the root.
-#     """
-#     goal_reward = root.goal_reward()
-#     if goal_reward is not None:
-#         return goal_reward
-
-#     if root.has_unvisited_children():
-#         child = choose_unvisited(root)
-#         reward = rollout(child)
-#         child.q_value += reward
-#         child.num_visits += 1
-#         update_regret(root, reward)
-#         return return_value(reward, child.q_value, child.num_visits)
-
-#     else:
-#         child = select(root)
-#         reward = iterate(child)
-#         child.q_value += reward
-#         child.num_visits += 1
-#         update_regret(root, reward)
-#         return return_value(reward, child.q_value, child.num_visits)
 
 
 def rollout(node: "Node"):
@@ -177,7 +117,6 @@ def rollout_policy(game_state: GameState) -> GameState:
     new_state = game_state.copy()
     new_state.update(friend_choice, enemy_choice)
     return new_state
-    # return game_state.update(enemy_choice, friend_choice)
 
 
 def pick_unvisited_child(node: Node) -> Node:
@@ -186,14 +125,6 @@ def pick_unvisited_child(node: Node) -> Node:
     Possible to use a heuristic here instead of randomness.
     """
     unvisited = node.unvisited_children()
-    # available = [(i, j)
-    #              for i in range(len(root.friend_transitions))
-    #              for j in range(len(root.enemy_transitions))
-    #              if root.matrix[i][j] is None]
-
-    # (i, j) = available[randrange(len(available))]
-    # child = new_node(root, i, j)
-    # return child
     return unvisited[randrange(len(unvisited))]
 
 
@@ -201,27 +132,10 @@ def update_regret(node: Node, reward):
     """
     Update the regret array of the particular node, taking the new reward into account
     """
-    # if node.parent is not None:
-    #     for j, enemy_transition in enumerate(node.parent.enemy_transitions):
-    #         if enemy_transition == node.
-
     for i in range(len(node.parent.matrix)):
         if i != node.i:
             node.parent.regret[i] += node.parent.matrix[i][node.j].q_value - reward
 
-    # for friend_index in range(len(node.friend_transitions)):
-    #     if 
-    #     node.regret[friend_index] += (regret_value(root, friend_index, reward) - reward)
-
-
-# def regret_value(node: Node, i, reward):
-#     """
-#     Return regret value based on friend index
-#     """
-#     if node.i == i:
-#         return reward
-#     else:
-#         return node.q_value
 
 
 def get_best_child(root: Node):
@@ -244,12 +158,12 @@ def get_best_child(root: Node):
     i = choice(range(len(root.friend_transitions)), p=probabilities)
     j = choice(range(len(root.enemy_transitions)))
 
-    # if root.matrix[i][j] is None:
-    #     # root.matrix[i][j] = new_node(root, root.friend_transitions[i], root.enemy_transitions[j])
-    #     root.matrix[i][j] = root.make_updated_node(root.friend_transitions[i], root.enemy_transitions[j], root)
     return root.matrix[i][j]
 
 def choose_winner(root: Node):
+    """
+    Choose overall winning move based on mixed strategy output
+    """
     # TODO These probabilities probably need to be a part of node and updated iteratively somewhere
     # else in the code
     probabilities = []
@@ -260,7 +174,6 @@ def choose_winner(root: Node):
 
     return root.friend_transitions[i]
 
-    pass
 
 def strategy(root: Node, regret_index):
     """
@@ -276,17 +189,6 @@ def strategy(root: Node, regret_index):
         return max(root.regret[regret_index], 0) / regret_sum
     else:
         return 1 / len(root.friend_transitions)
-
-
-# def return_value(reward, q_value, num_visits):
-#     """
-#     Return value.
-
-#     Not sure of the purpose of this since the number can be calculated at any point, 
-#     needs investigating
-#     """
-#     # TODO Investigate the purpose of this return value in the paper.
-#     return q_value / num_visits
 
 
 def add_children(node: Node):
@@ -307,28 +209,6 @@ def add_children(node: Node):
             for i, friend_transition in enumerate(node.friend_transitions)
         ]
 
-# # def new_node(parent: Node, friend_index, enemy_index) -> Node:
-# def new_node(root: Node, friend_transition, enemy_transition):
-#     """
-#     Create a new node and initialize it.
-
-#     Connects parent and child, finds possible moves, etc.
-#     """
-#     # friend_transition = parent.friend_transitions[friend_index]
-#     # enemy_transition = parent.enemy_transitions[enemy_index]
-
-#     child = root.make_updated_node(friend_transition, enemy_transition, root)
-#     # # child = Node(parent.update(enemy_transition, friend_transition))
-#     # # child.parent = parent
-#     # # child.i = friend_index
-#     # # child.j = enemy_index
-#     # # parent.matrix[friend_index][enemy_index] = child
-
-#     # # child.init_transitions()
-#     # child.action = (
-#     #     parent.friend_transitions[friend_index], parent.enemy_transitions[enemy_index])
-#     # return child
-
 
 def test():
     """
@@ -342,8 +222,5 @@ def test():
     node.enemies = [('s', (-4, 0)), ('p', (0, 4))]
     node.friend_transitions = node.next_friend_transitions()
     node.enemy_transitions = node.next_enemy_transitions()
-    # node.init_matrix()
-    # for _ in range(1000):
-    #     iterate(node)
 
     ad = 98
